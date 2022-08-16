136dc75f40b514a8b26a8048076da8fe4cd438c7
Duc Nguyen <duc.dustinnguyen@gmail.com>
2022-08-16 00:24:33 -0400

    inital commit

---
+++ src/main/java/com/mvc/bugtracker/BugTrackerApplication.java
@@ -0,0 +1,14 @@
+package com.mvc.bugtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude= {SecurityAutoConfiguration.class})
+public class BugTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BugTrackerApplication.class, args);
+		System.out.println("Sever Bug Tracker application started");
+	}
+}
